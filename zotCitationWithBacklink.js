711b10895ebaf82cb41857c7082b989f0da2b7a1
rubisco57 <rubisco57@gmail.com>
2020-01-17 17:48:37 +0100

    Update zotCitationWithBacklink.js

--- zotCitationWithBacklink.js
+++ zotCitationWithBacklink.js
@@ -15,48 +15,48 @@
 function doExport() {
 	var item;
 	while(item = Zotero.nextItem()) {
-			var urlS;
-			// Error: isRegularItem() is not a function
-			if (item.isRegularItem()) { // not an attachment already
-		    let attachmentIDs = item.getAttachments();
-		    for (let id of attachmentIDs) {
-		        let attachment = Zotero.Items.get(id);
-		        if (attachment.attachmentContentType == 'application/pdf') {
-								var library_id = attachment.libraryID ? attachment.libraryID : 0;
-		            // Write zotero://open-pdf/library/items/[itemKey]?page=[page]
-								urlS = '<a href="zotero://open-pdf/library/items/'+library_id+'_'+attachment.key+'?page=1">';
-								break;
-		        }
-		    }
-			}
-			if (urlS.isEmpty()) {
-				var library_id = item.libraryID ? item.libraryID : 0;
-				urlS =  '<a href="zotero://open-pdf/library/items/'+library_id+'_'+item.key+'">';
-			}
-			Zotero.write(urlS);
-			// Zotero.write('<a href="zotero://select/items/');
-			// var library_id = item.libraryID ? item.libraryID : 0;
-
-			var titleS = (item.title) ? item.title.replace(/&/g,'&amp;').replace(/"/g,'&quot;') : "(no title)";
-			// var pubTitleS = (item.publicationTitle) ? item.publicationTitle.replace(/&/g,'&amp;').replace(/"/g,'&quot;') : "";
-			// if (!pubTitleS && item.type)
-			// 	pubTitleS = '['+item.type+']';
-			// Zotero.write(library_id+'_'+item.key+'" title="'+titleS+'&#13;'+((item.conferenceName) ? item.conferenceName : pubTitleS)+'">');
-
-			var creatorsS = "";
-			if (item.creators.length != 0) {
-				creatorsS = item.creators[0].lastName
-				if (item.creators.length>2)
-					creatorsS += " et al.";
-				else if (item.creators.length==2)
-					creatorsS += " &amp; " + item.creators[1].lastName;
-			} else if (item.itemType == "webpage" ) {
-				creatorsS = item.websiteTitle
-			}
-
-			var date = Zotero.Utilities.strToDate(item.date);
-			var dateS = (date.year) ? date.year : item.date;
-
-			Zotero.write(creatorsS + '. ' + titleS + '. ' + dateS + '. ' + '</a><br/>');
+		var urlS;
+		// Error: isRegularItem() is not a function
+		if (item.isRegularItem()) { // not an attachment already
+	    let attachmentIDs = item.getAttachments();
+	    for (let id of attachmentIDs) {
+	        let attachment = Zotero.Items.get(id);
+	        if (attachment.attachmentContentType == 'application/pdf') {
+							var library_id = attachment.libraryID ? attachment.libraryID : 0;
+	            // Write zotero://open-pdf/library/items/[itemKey]?page=[page]
+							urlS = '<a href="zotero://open-pdf/library/items/'+library_id+'_'+attachment.key+'?page=1">';
+							break;
+	        }
+	    }
 		}
+		if (urlS.isEmpty()) {
+			var library_id = item.libraryID ? item.libraryID : 0;
+			urlS =  '<a href="zotero://open-pdf/library/items/'+library_id+'_'+item.key+'">';
+		}
+		Zotero.write(urlS);
+		// Zotero.write('<a href="zotero://select/items/');
+		// var library_id = item.libraryID ? item.libraryID : 0;
+
+		var titleS = (item.title) ? item.title.replace(/&/g,'&amp;').replace(/"/g,'&quot;') : "(no title)";
+		// var pubTitleS = (item.publicationTitle) ? item.publicationTitle.replace(/&/g,'&amp;').replace(/"/g,'&quot;') : "";
+		// if (!pubTitleS && item.type)
+		// 	pubTitleS = '['+item.type+']';
+		// Zotero.write(library_id+'_'+item.key+'" title="'+titleS+'&#13;'+((item.conferenceName) ? item.conferenceName : pubTitleS)+'">');
+
+		var creatorsS = "";
+		if (item.creators.length != 0) {
+			creatorsS = item.creators[0].lastName
+			if (item.creators.length>2)
+				creatorsS += " et al.";
+			else if (item.creators.length==2)
+				creatorsS += " &amp; " + item.creators[1].lastName;
+		} else if (item.itemType == "webpage" ) {
+			creatorsS = item.websiteTitle
+		}
+
+		var date = Zotero.Utilities.strToDate(item.date);
+		var dateS = (date.year) ? date.year : item.date;
+
+		Zotero.write(creatorsS + '. ' + titleS + '. ' + dateS + '. ' + '</a><br/>');
+	}
 }
